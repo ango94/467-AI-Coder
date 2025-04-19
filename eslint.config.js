ab2085ef4a2cc6219686b299e399b31b0c142f33
BrianAnderson <anderbr4@oregonstate.edu>
2025-04-19 15:30:12 -0500

    Add .gitignore file to exclude node_modules and other unnecessary files

--- eslint.config.js
+++ eslint.config.js
@@ -1,12 +1,31 @@
+import react from "eslint-plugin-react";
+import reactHooks from "eslint-plugin-react-hooks";
+import babelParser from "@babel/eslint-parser";
+
 export default [
   {
     files: ["**/*.js", "**/*.jsx"],
     ignores: ["node_modules/**"],
+    languageOptions: {
+      parser: babelParser,
+      parserOptions: {
+        ecmaVersion: 2021,
+        sourceType: "module",
+        ecmaFeatures: {
+          jsx: true, // Enable JSX parsing
+        },
+      },
+    },
+    plugins: {
+      react,
+      reactHooks,
+    },
     rules: {
-      "no-unused-vars": "warn",
-      "no-console": "off",
-      "semi": ["error", "always"],
-      "quotes": ["error", "double"],
+      "react/jsx-uses-react": "error",
+      "react/jsx-uses-vars": "error",
+      "react/react-in-jsx-scope": "off", // Not needed for React 17+
+      "react-hooks/rules-of-hooks": "error",
+      "react-hooks/exhaustive-deps": "warn",
     },
   },
 ];
