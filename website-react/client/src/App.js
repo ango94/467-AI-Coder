98e57c65d01d6c9ae8d3587c1cf81727d4b6d27c
ango94 <ngoalex@oregonstate.edu>
2025-05-31 11:17:00 -0700

    added front end for tests and footer for swapping

--- website-react/client/src/App.js
+++ website-react/client/src/App.js
@@ -3,27 +3,33 @@ import Login from './components/Login';
 import Register from './components/Register';
 import Todo from './components/Todo';
 import AdminDashboard from './components/AdminDashboard';
+import TestRunner from './components/TestRunner'
+import Footer from './components/Footer';
 
 function App() {
   const user = JSON.parse(localStorage.getItem('user'));
 
   return (
-    <Routes>
-      <Route
-        path="/"
-        element={
-          user ? (
-            <Navigate to={user.role === 'admin' ? '/admin' : '/todo'} />
-          ) : (
-            <Navigate to="/login" />
-          )
-        }
-      />
-      <Route path="/login" element={<Login />} />
-      <Route path="/register" element={<Register />} />
-      <Route path="/todo" element={<Todo />} />
-      <Route path="/admin" element={<AdminDashboard />} />
-    </Routes>
+    <>
+      <Routes>
+        <Route
+          path="/"
+          element={
+            user ? (
+              <Navigate to={user.role === 'admin' ? '/admin' : '/todo'} />
+            ) : (
+              <Navigate to="/login" />
+            )
+          }
+        />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/todo" element={<Todo />} />
+        <Route path="/admin" element={<AdminDashboard />} />
+        <Route path="/test" element={<TestRunner />} />
+      </Routes>
+      <Footer />
+    </>
   );
 }
 
