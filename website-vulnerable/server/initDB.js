79daffb98dabdcf72cbd2919cec5da0dfc43ffb7
Sam <slevy239@gmail.com>
2025-05-31 17:32:09 -0400

    comment

--- website-vulnerable/server/initDB.js
+++ website-vulnerable/server/initDB.js
@@ -31,8 +31,8 @@ async function initDatabase() {
     const projectClient = new Client({
       user: process.env.DB_USER,
       password: process.env.DB_PASSWORD,
-      host: process.env.DB_HOST,
-      port: process.env.DB_PORT,
+      // host: process.env.DB_HOST,
+      // port: process.env.DB_PORT,
       database: process.env.DB_NAME
     });
 
